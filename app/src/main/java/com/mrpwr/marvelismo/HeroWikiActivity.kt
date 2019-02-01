2f464201fa515cc0fb6ca7287ad7ba78520d9466
unknown <frans.herrstrom@gmail.com>
2019-02-01 18:17:16 +0100

    webview settings

--- app/src/main/java/com/mrpwr/marvelismo/HeroWikiActivity.kt
+++ app/src/main/java/com/mrpwr/marvelismo/HeroWikiActivity.kt
@@ -1,7 +1,10 @@
 package com.mrpwr.marvelismo
 
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_hero_wiki.*
@@ -15,6 +18,22 @@ class HeroWikiActivity : AppCompatActivity() {
       var webView:WebView=findViewById(R.id.heroWikiWeb)
         webView.webViewClient = WebViewClient()
         webView.settings.javaScriptEnabled = true
+        webView.settings.loadWithOverviewMode = true
+        webView.settings.useWideViewPort = true
+       webView.settings.setSupportZoom(true)
+        webView.settings.builtInZoomControls = false
+        webView.settings.layoutAlgorithm = WebSettings.LayoutAlgorithm.SINGLE_COLUMN
+        webView.settings.cacheMode = WebSettings.LOAD_NO_CACHE
+        webView.settings.domStorageEnabled = true
+        webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        webView.setScrollbarFadingEnabled(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        } else {
+            webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
+
+
 
         val url:String= intent.getStringExtra("WIKI_URL")
 
