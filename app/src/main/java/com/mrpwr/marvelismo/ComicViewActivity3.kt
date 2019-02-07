29dc7b0b6815dcd80e447bef5aca9d5d08a785fa
unknown <frans.herrstrom@gmail.com>
2019-02-07 10:37:55 +0100

    fixed comic view for ali boulala

--- app/src/main/java/com/mrpwr/marvelismo/ComicViewActivity3.kt
+++ app/src/main/java/com/mrpwr/marvelismo/ComicViewActivity3.kt
@@ -1,7 +1,12 @@
 package com.mrpwr.marvelismo
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 class ComicViewActivity3 : AppCompatActivity() {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.comic_view_activity3)
+        println("THIS IS THE COMIC VIEW ACTIVITY")
+    }
 }
